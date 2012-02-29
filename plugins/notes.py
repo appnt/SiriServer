29e6c58a986f15490a9b0c2a713ab483920ea677
appnt <totonico@yahoo.fr>
2012-03-01 00:09:29 +0100

    french translations for notes.py

--- plugins/notes.py
+++ plugins/notes.py
@@ -47,14 +47,16 @@ class Create(ClientBoundCommand):
 
 class note(Plugin):
     localizations = {"noteDefaults": 
-                        {"searching":{"en-US": "Creating your note ..."}, 
-                         "result": {"en-US": "Here is your note:"},
-                         "nothing": {"en-US": "What should I note?"}}, 
+                        {"searching":{"en-US": "Creating your note ...", "fr-FR": u"Création de votre note ..."}, 
+                         "result": {"en-US": "Here is your note:", "fr-FR": "Voici votre note:"},
+                         "nothing": {"en-US": "What should I note?", "fr-FR": "Que dois-je noter?"}}, 
                     "failure": {
-                                "en-US": "I cannot type your note right now."
+                                "en-US": "I cannot type your note right now.",
+                                "fr-FR": u"Je ne peux pas créer de note actuellement."
                                 }
                     }
     @register("en-US", "(.*note [a-zA-Z0-9]+)|(.*create.*note [a-zA-Z0-9]+)|(.*write.*note [a-zA-Z0-9]+)")
+    @register("fr-FR", u"(.*note [a-zA-Z0-9]+)|(.*cré*.*note [a-zA-Z0-9]+)|(.*écrire.*note [a-zA-Z0-9]+)")
     def writeNote(self, speech, language):
         content_raw = re.match(".*note ([a-zA-Z0-9, ]+)$", speech, re.IGNORECASE)
         if content_raw == None:
